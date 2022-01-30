1479e759459d9676b508a37928b379996bb75ca0
alivething <kaustubhkhedkar02@gmail.com>
2022-01-30 13:55:07 +0530

    Web Frontend using streamlit

--- web.py
+++ web.py
@@ -6,30 +6,33 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
 st.title("Stock Prediction")
-stocks = ("IBM", "DaiDex", "GPVTRV", "Reliance", "ShopTRT", "SAIC", "TSCOLON", "VANKE")
+stocks = ("IBM", "DaiDex", "Reliance", "SAIC", "TSCOLON", "VANKE")
 selected_stocks = st.selectbox("Select stock fpr prediction", stocks)
 
 if(selected_stocks=="IBM"):
     model=load_model("ibmstockrev.h5")
     dataset = pd.read_csv('daily_IBM.csv')
+
 if(selected_stocks=="DaiDex"):
     model=load_model("daidexstockrev.h5")
     dataset = pd.read_csv('daily_DAIDEX.csv')
-if(selected_stocks=="GPVTRV"):
-    model=load_model("gpvtrvstockrev.h5")
-    dataset = pd.read_csv('daily_GPVTRV.csv')
+
 if(selected_stocks=="Reliance"):
     model=load_model("reliancestockrev.h5")
     dataset = pd.read_csv('daily_RELIANCEBSE.csv')
+
 if(selected_stocks=="ShopTRT"):
     model=load_model("shopstrtstockrev.h5")
     dataset = pd.read_csv('daily_SHOPTRT.csv')
+
 if(selected_stocks=="SAIC"):
     model=load_model("saicshhstockrev.h5")
     dataset = pd.read_csv('daily_SIACSHH.csv')
+
 if(selected_stocks=="TSCOLON"):
     model=load_model("tscolonstockrev.h5")
     dataset = pd.read_csv('daily_TSCOLON.csv')
+
 if(selected_stocks=="VANKE"):
     model=load_model("vankestockrev.h5")
     dataset = pd.read_csv('daily_VANKE.csv')
@@ -40,8 +43,8 @@ testing_set_scaled = sc.fit_transform(dataset)
 X_test = []
 y_test = []
 
-X_test.append(testing_set_scaled[0:60])
-y_test.append(testing_set_scaled[0])
+X_test.append(testing_set_scaled[:60])
+y_test= dataset[0:5]
 
 X_test = np.array(X_test)
 X_test = X_test.reshape(1, 60, 1)
@@ -51,5 +54,5 @@ y_pred = sc.inverse_transform(y_pred)
 st.subheader('Forecasted Close Value')
 st.write(y_pred)
 
-st.subheader('Previous Day Close Value')    
-st.write(sc.inverse_transform(y_test))
+st.subheader('Previous 5 Close Value')    
+st.write(y_test)
